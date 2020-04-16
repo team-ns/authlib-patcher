ae4c90eadfeeb760b5253b263afa313a0041fe32
RussianJeb <russianjeb@protonmail.com>
2020-04-16 13:01:36 +0300

    Initial Commit

---
+++ src/main/java/com/nslauncher/patcher/AuthlibPatcher.java
@@ -0,0 +1,135 @@
+package com.nslauncher.patcher;
+
+
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import org.objectweb.asm.*;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * @author Jeb
+ */
+public class AuthlibPatcher {
+
+    public static void main(String[] args) throws Exception {
+        String url = args[1];
+        String jar = args[0];
+        byte[] bytes = getBytes("com/mojang/authlib/yggdrasil/YggdrasilMinecraftSessionService.class", jar);
+        saveBytes(jar, "com/mojang/authlib/yggdrasil/YggdrasilMinecraftSessionService.class", transformSessionService(bytes, url));
+        bytes = getBytes("com/mojang/authlib/properties/Property.class", jar);
+        saveBytes(jar, "com/mojang/authlib/properties/Property.class", transformCheckSignature(bytes));
+    }
+
+    private static byte[] transformCheckSignature(byte[] bytes) {
+        ClassReader classReader = new ClassReader(bytes);
+        ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+        classReader.accept(new ClassVisitor(Opcodes.ASM5, classWriter) {
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
+                if ("isSignatureValid".equals(name)) {
+                    return new Visitor(visitor);
+                }
+                return visitor;
+            }
+        }, 0);
+        return classWriter.toByteArray();
+    }
+
+    private static byte[] transformSessionService(byte[] bytes, String url) {
+        ClassReader classReader = new ClassReader(bytes);
+        ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+        ClassNode classNode = new ClassNode();
+        classReader.accept(classNode, 0);
+        classNode.methods.stream()
+                .filter(methodNode -> methodNode.name.equals("<clinit>"))
+                .forEach(methodNode -> methodNode.instructions.forEach(ins -> {
+                    if (ins.getOpcode() == Opcodes.LDC) {
+                        LdcInsnNode ldcInsnNode = (LdcInsnNode) ins;
+                        if (ins.getNext().getNext().getOpcode() == Opcodes.PUTSTATIC) {
+                            FieldInsnNode fieldInsnNode = (FieldInsnNode) ins.getNext().getNext();
+                            if (fieldInsnNode.name.equals("CHECK_URL")) {
+                                ldcInsnNode.cst = url + "/join";
+                            } else if (fieldInsnNode.name.equals("JOIN_URL")) {
+                                ldcInsnNode.cst = url + "/hasJoined";
+                            }
+                        }
+                    }
+                }));
+        classNode.accept(new ClassVisitor(Opcodes.ASM5, classWriter) {
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
+                if ("isWhitelistedDomain".equals(name)) {
+                    return new Visitor(visitor);
+                }
+                return visitor;
+            }
+        });
+        return classWriter.toByteArray();
+    }
+
+    private static void saveBytes(String jar, String javaFileName, byte[] bytes) throws ZipException {
+        ZipFile file = new ZipFile(jar);
+        ZipParameters zipParameters = new ZipParameters();
+        zipParameters.setFileNameInZip(javaFileName);
+        file.addStream(new ByteArrayInputStream(bytes), zipParameters);
+    }
+
+    private static byte[] getBytes(String javaFileName, String jar) throws IOException {
+        try (JarFile jarFile = new JarFile(jar)) {
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (entry.getName().endsWith(".class") &&
+                        entry.getName().equals(javaFileName)) {
+                    try (InputStream inputStream = jarFile.getInputStream(entry)) {
+                        return getBytes(inputStream);
+                    } catch (IOException ioException) {
+                        System.out.println("Could not obtain class entry for " + entry.getName());
+                        throw ioException;
+                    }
+                }
+            }
+        }
+        throw new IOException("File not found");
+    }
+
+    private static byte[] getBytes(InputStream is) throws IOException {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[0xFFFF];
+            for (int len; (len = is.read(buffer)) != -1; )
+                os.write(buffer, 0, len);
+            os.flush();
+            return os.toByteArray();
+        }
+    }
+
+    public static class Visitor extends MethodVisitor {
+        private MethodVisitor visitor;
+
+        public Visitor(MethodVisitor visitor) {
+            super(Opcodes.ASM5);
+            this.visitor = visitor;
+        }
+
+        @Override
+        public void visitCode() {
+            visitor.visitCode();
+            visitor.visitInsn(Opcodes.ICONST_1);
+            visitor.visitInsn(Opcodes.IRETURN);
+            visitor.visitEnd();
+        }
+    }
+
+}
