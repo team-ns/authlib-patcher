1c26f80f0d54879d8774a6ea265242b9873c3c3a
RussianJeb <russianjeb@protonmail.com>
2020-10-30 22:23:16 +0300

    Patch YggdrasilMinecraftSessionService for new launcher auth

--- src/main/java/com/nslauncher/patcher/transformers/yaggdrasil/MinecraftSessionTransformer.java
+++ src/main/java/com/nslauncher/patcher/transformers/yaggdrasil/MinecraftSessionTransformer.java
@@ -28,7 +28,12 @@ public class MinecraftSessionTransformer extends Transformer {
             methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC, "proxyUrl", "(Ljava/lang/String;)Ljava/net/URL;", null, null);
             makeProxy(methodVisitor, config);
         }
-        classNode.accept(new ClassVisitor(Opcodes.ASM5, classWriter) {
+        if (classNode.methods.stream().noneMatch(methodNode -> methodNode.name.equals("launcherJoinRequest"))) {
+            MethodVisitor methodVisitor;
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_NATIVE, "launcherJoinRequest", "(Lcom/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest;)V", null, null);
+            methodVisitor.visitEnd();
+        }
+        classNode.accept(new ClassVisitor(ASM5, classWriter) {
             @Override
             public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
                 MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
@@ -40,6 +45,9 @@ public class MinecraftSessionTransformer extends Transformer {
                 }
                 if ("isWhitelistedDomain".equals(name)) {
                     return new MethodTrueVisitor(visitor);
+                }
+                if ("joinServer".equals(name)) {
+                    return new JoinServerVisitor(visitor);
                 }
                 if ("proxyUrl".equals(name)) {
                     return new ProxyVisitor(visitor, config);
@@ -79,9 +87,11 @@ public class MinecraftSessionTransformer extends Transformer {
         methodVisitor.visitMaxs(2, 1);
         methodVisitor.visitEnd();
     }
+
     public static class ProxyVisitor extends MethodVisitor {
         private final Config config;
         private final MethodVisitor methodVisitor;
+
         public ProxyVisitor(MethodVisitor visitor, Config config) {
             super(ASM5);
             this.config = config;
@@ -135,5 +145,65 @@ public class MinecraftSessionTransformer extends Transformer {
 
         }
 
+    }
+
+
+    public static class JoinServerVisitor extends MethodVisitor {
+        private final MethodVisitor methodVisitor;
+
+        public JoinServerVisitor(MethodVisitor methodVisitor) {
+            super(ASM5, null);
+            this.methodVisitor = methodVisitor;
+        }
+
+        @Override
+        public void visitCode() {
+            methodVisitor.visitCode();
+            Label label0 = new Label();
+            methodVisitor.visitLabel(label0);
+            methodVisitor.visitLineNumber(78, label0);
+            methodVisitor.visitTypeInsn(NEW, "com/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest");
+            methodVisitor.visitInsn(DUP);
+            methodVisitor.visitMethodInsn(INVOKESPECIAL, "com/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest", "<init>", "()V", false);
+            methodVisitor.visitVarInsn(ASTORE, 4);
+            Label label1 = new Label();
+            methodVisitor.visitLabel(label1);
+            methodVisitor.visitLineNumber(79, label1);
+            methodVisitor.visitVarInsn(ALOAD, 4);
+            methodVisitor.visitVarInsn(ALOAD, 2);
+            methodVisitor.visitFieldInsn(PUTFIELD, "com/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest", "accessToken", "Ljava/lang/String;");
+            Label label2 = new Label();
+            methodVisitor.visitLabel(label2);
+            methodVisitor.visitLineNumber(80, label2);
+            methodVisitor.visitVarInsn(ALOAD, 4);
+            methodVisitor.visitVarInsn(ALOAD, 1);
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/mojang/authlib/GameProfile", "getId", "()Ljava/util/UUID;", false);
+            methodVisitor.visitFieldInsn(PUTFIELD, "com/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest", "selectedProfile", "Ljava/util/UUID;");
+            Label label3 = new Label();
+            methodVisitor.visitLabel(label3);
+            methodVisitor.visitLineNumber(81, label3);
+            methodVisitor.visitVarInsn(ALOAD, 4);
+            methodVisitor.visitVarInsn(ALOAD, 3);
+            methodVisitor.visitFieldInsn(PUTFIELD, "com/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest", "serverId", "Ljava/lang/String;");
+            Label label4 = new Label();
+            methodVisitor.visitLabel(label4);
+            methodVisitor.visitLineNumber(83, label4);
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitVarInsn(ALOAD, 4);
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/mojang/authlib/yggdrasil/YggdrasilAuthenticationService", "launcherJoinRequest", "(Lcom/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest;)V", false);
+            Label label5 = new Label();
+            methodVisitor.visitLabel(label5);
+            methodVisitor.visitLineNumber(84, label5);
+            methodVisitor.visitInsn(RETURN);
+            Label label6 = new Label();
+            methodVisitor.visitLabel(label6);
+            methodVisitor.visitLocalVariable("this", "Lcom/mojang/authlib/yggdrasil/YggdrasilMinecraftSessionService;", null, label0, label6, 0);
+            methodVisitor.visitLocalVariable("profile", "Lcom/mojang/authlib/GameProfile;", null, label0, label6, 1);
+            methodVisitor.visitLocalVariable("authenticationToken", "Ljava/lang/String;", null, label0, label6, 2);
+            methodVisitor.visitLocalVariable("serverId", "Ljava/lang/String;", null, label0, label6, 3);
+            methodVisitor.visitLocalVariable("request", "Lcom/mojang/authlib/yggdrasil/request/JoinMinecraftServerRequest;", null, label1, label6, 4);
+            methodVisitor.visitMaxs(3, 5);
+            methodVisitor.visitEnd();
+        }
     }
 }
